383900655c481fbd13aee77515cc688c065ab413
Tehseen Ali <154703743+Tehseenali445@users.noreply.github.com>
2024-03-10 23:39:27 +0500

    Variables For Strings

---
+++ app.js
@@ -0,0 +1,56 @@
+// VARIABLES FOR STRINGS
+// Q1
+
+// let username;
+
+// Q2
+
+// let myname = "Tehseen Ali Siddiqui";
+// console.log(myname);
+
+// Q3
+
+// let message = "Hello World"
+// alert(message)
+// console.log(message)
+
+// Q4
+
+// let username = "Tehseen Ali Siddiqui";
+// alert(username);
+// console.log(username);
+
+// let user_age = "My Age Is 26 Year";
+// alert(user_age);
+// console.log(user_age);
+
+// let detail = "Certified Mobile Applicaton Development";
+// alert(detail);
+// console.log(detail);
+
+// Q5
+
+// let piza = "PIZZA \nPIZZ\nPIZ\nPI\nP";
+// alert(piza)
+
+// Q6
+
+// let email = "My E-mail is (Tehseenali445@gmail.com)";
+// alert(email)
+
+// Q7
+
+// let book =
+//   "I am trying to learn from the Book A smarter \nway to learn 'JavaScript'";
+// alert(book);
+
+
+// Q8
+
+// let para = "<h1>Yah! I Can write HTML content through JavaScript<h1/>"
+// document.write(para)
+
+// Q9
+
+// let design = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬"
+// alert(design);
